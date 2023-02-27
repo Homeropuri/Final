474c9b45c4e4fd67d0636897df1e5b80b7b27d6a
Homero <homeropuricelli@gmail.com>
2023-02-27 19:13:29 -0300

    Subo el archivo

---
+++ Final.js
@@ -0,0 +1,115 @@
+//Educación//
+document.getElementById('Educacion1').addEventListener('click', function() {
+    document.getElementById('Ed1').innerHTML = "Bachiller en Ciencias Sociales"
+    document.getElementById('Ed2').innerHTML = "Colegio del Centenario"
+    document.getElementById('Ed3').innerHTML = "Primaria: 2008-2013 / Secundaria: 2014-2019"
+    document.getElementById('Ed4').innerHTML = "Profesorado de Educacion Fisica"
+    document.getElementById('Ed5').innerHTML = "Universidad Nacional de La Plata"
+    document.getElementById('Ed6').innerHTML = "2020-2021"
+    document.getElementById('Ed7').innerHTML = "Licenciatura en sistema"
+    document.getElementById('Ed8').innerHTML = "Universidad Nacional de La Plata"
+    document.getElementById('Ed9').innerHTML = "2022-Actualidad"
+    document.getElementById('Educacion1').innerHTML = ""
+    document.getElementById('Educacion2').innerHTML = "Cerrar"
+})
+
+    document.getElementById('Educacion2').addEventListener('click', function() {
+        document.getElementById('Ed1').innerHTML = ""
+        document.getElementById('Ed2').innerHTML = ""
+        document.getElementById('Ed3').innerHTML = ""
+        document.getElementById('Ed4').innerHTML = ""
+        document.getElementById('Ed5').innerHTML = ""
+        document.getElementById('Ed6').innerHTML = ""
+        document.getElementById('Ed7').innerHTML = ""
+        document.getElementById('Ed8').innerHTML = ""
+        document.getElementById('Ed9').innerHTML = ""
+        document.getElementById('Educacion1').innerHTML = "Mostrar"
+        document.getElementById('Educacion2').innerHTML = ""
+
+    })
+
+
+//Habilidades//
+document.getElementById('Habilidades1').addEventListener('click', function() {
+    document.getElementById('Ha1').innerHTML = "Creatividad"
+    document.getElementById('Ha2').innerHTML = "Responsabilidad"
+    document.getElementById('Ha3').innerHTML = "Puntualidad"
+    document.getElementById('Ha4').innerHTML = "Manejo de computadoras"
+    document.getElementById('Ha5').innerHTML = "Trabajos de carpinteria y electricidad"
+    document.getElementById('Habilidades2').innerHTML = "Cerrar"
+    document.getElementById('Habilidades1').innerHTML = ""
+})
+
+document.getElementById('Habilidades2').addEventListener('click', function() {
+    document.getElementById('Ha1').innerHTML = ""
+    document.getElementById('Ha2').innerHTML = ""
+    document.getElementById('Ha3').innerHTML = ""
+    document.getElementById('Ha4').innerHTML = ""
+    document.getElementById('Ha5').innerHTML = ""
+    document.getElementById('Habilidades1').innerHTML = "Mostrar"
+    document.getElementById('Habilidades2').innerHTML = ""
+})
+
+
+//Experiencia profesional//
+document.getElementById('Experiencia profesional1').addEventListener('click', function() {
+    document.getElementById('EP1').innerHTML = "Roster Basket"
+    document.getElementById('EP2').innerHTML = "Rol: Arbitro"
+    document.getElementById('EP3').innerHTML = "2022"
+    document.getElementById('EP4').innerHTML = "Sound Group DJ's"
+    document.getElementById('EP5').innerHTML = "Rol: Translados y armados de eventos"
+    document.getElementById('EP6').innerHTML = "2022-Actualidad"
+    document.getElementById('Experiencia profesional2').innerHTML = "Cerrar"
+    document.getElementById('Experiencia profesional1').innerHTML = ""
+})
+
+document.getElementById('Experiencia profesional2').addEventListener('click', function() {
+    document.getElementById('EP1').innerHTML = ""
+    document.getElementById('EP2').innerHTML = ""
+    document.getElementById('EP3').innerHTML = ""
+    document.getElementById('EP4').innerHTML = ""
+    document.getElementById('EP5').innerHTML = ""
+    document.getElementById('EP6').innerHTML = ""
+    document.getElementById('Experiencia profesional1').innerHTML = "Mostrar"
+    document.getElementById('Experiencia profesional2').innerHTML = ""
+})
+
+
+//Idiomas//
+document.getElementById('Idiomas1').addEventListener('click', function() {
+    document.getElementById('Id1').innerHTML = "Español (Nativo)"
+    document.getElementById('Id2').innerHTML = "Ingles (Basico)"
+    document.getElementById('Idiomas2').innerHTML = "Cerrar"
+    document.getElementById('Idiomas1').innerHTML = ""
+})
+
+document.getElementById('Idiomas2').addEventListener('click', function() {
+    document.getElementById('Id1').innerHTML = ""
+    document.getElementById('Id2').innerHTML = ""
+    document.getElementById('Idiomas1').innerHTML = "Mostrar"
+    document.getElementById('Idiomas2').innerHTML = ""
+})
+
+
+//Intereses//
+document.getElementById('Intereses1').addEventListener('click', function() {
+    document.getElementById('Int1').innerHTML = "Tecnologia"
+    document.getElementById('Int2').innerHTML = "Deportes"
+    document.getElementById('Int3').innerHTML = "Lectura"
+    document.getElementById('Int4').innerHTML = "Musica"
+    document.getElementById('Int5').innerHTML = "Carpinteria"
+    document.getElementById('Int6').innerHTML = "Electricidad"
+    document.getElementById('Intereses2').innerHTML = "Cerrar"
+    document.getElementById('Intereses1').innerHTML = ""
+})
+
+document.getElementById('Intereses2').addEventListener('click', function() {
+    document.getElementById('Int1').innerHTML = ""
+    document.getElementById('Int2').innerHTML = ""
+    document.getElementById('Int3').innerHTML = ""
+    document.getElementById('Int4').innerHTML = ""
+    document.getElementById('Int5').innerHTML = ""
+    document.getElementById('Int6').innerHTML = ""
+    document.getElementById('Intereses1').innerHTML = "Mostrar"
+    document.getElementById('Intereses2').innerHTML = ""
+})
